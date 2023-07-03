e3b612b2fdff2cbdc53fd2978fff2da8d2b40f8b
Ulli Goschler <git@nce.wtf>
2023-07-03 10:28:41 +0200

    force update

--- comtrya/nvim/files/symlink/after/plugin/neotree.lua
+++ comtrya/nvim/files/symlink/after/plugin/neotree.lua
@@ -1,4 +1,4 @@
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 vim.keymap.set("n", "<leader>b", "<cmd>NeoTreeFocusToggle<CR>")
-vim.keymap.set("n", "<leader>r", "<cmd>NeoTreeRevealToggle<CR>")
+vim.keymap.set("n", "<leader>r", "<cmd>NeoTreeReveal<CR>")
