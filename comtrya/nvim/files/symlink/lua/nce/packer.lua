1845301cac10ca130c1f3323baf35f30289f7fbe
Ulli Goschler <git@nce.wtf>
2024-02-08 09:44:39 +0100

    add noice/add kubectl plugins

--- comtrya/nvim/files/symlink/lua/nce/packer.lua
+++ comtrya/nvim/files/symlink/lua/nce/packer.lua
@@ -42,13 +42,21 @@ return require('packer').startup(function(use)
   })
 
   use {
+    "folke/noice.nvim",
+    requires = {
+      {'MunifTanjim/nui.nvim'},
+      {'rcarriga/nvim-notify'},
+    }
+  }
+
+  use {
     'VonHeikemen/lsp-zero.nvim',
     requires = {
       -- LSP Support
       {'neovim/nvim-lspconfig'},
       {'williamboman/mason.nvim'},
       {'williamboman/mason-lspconfig.nvim'},
-    
+
       -- Autocompletion
       {'hrsh7th/nvim-cmp'},
       {'hrsh7th/cmp-buffer'},
@@ -56,7 +64,7 @@ return require('packer').startup(function(use)
       {'saadparwaiz1/cmp_luasnip'},
       {'hrsh7th/cmp-nvim-lsp'},
       {'hrsh7th/cmp-nvim-lua'},
-    
+
       -- Snippets
       {'L3MON4D3/LuaSnip'},
       {'rafamadriz/friendly-snippets'},
@@ -66,12 +74,14 @@ return require('packer').startup(function(use)
   -- Git
   use { 'tpope/vim-fugitive' }
   use { 'tpope/vim-rhubarb' }
-  use { 'airblade/vim-gitgutter' }
+  -- use { 'airblade/vim-gitgutter' }
+  use { 'lewis6991/gitsigns.nvim' }
 
   -- Visual
   use { 'folke/zen-mode.nvim' }
   use { 'feline-nvim/feline.nvim' }
   use { 'pearofducks/ansible-vim' }
+  use { 'ntpeters/vim-better-whitespace' }
 
   -- npm install -g @compodoc/live-server
   use { 'davidgranstrom/nvim-markdown-preview' }
@@ -82,7 +92,7 @@ return require('packer').startup(function(use)
   use {
   "nvim-neo-tree/neo-tree.nvim",
     branch = "v2.x",
-    requires = { 
+    requires = {
       "MunifTanjim/nui.nvim",
     },
       require("neo-tree").setup({
