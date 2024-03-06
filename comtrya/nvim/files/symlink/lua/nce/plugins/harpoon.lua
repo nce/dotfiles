5a89ed3164d22e81340b81c6afd5fe303932f8bf
Ulli Goschler <git@nce.wtf>
2024-03-06 13:20:08 +0100

    use harpoon keys with qmk thumbkeys

--- comtrya/nvim/files/symlink/lua/nce/plugins/harpoon.lua
+++ comtrya/nvim/files/symlink/lua/nce/plugins/harpoon.lua
@@ -4,11 +4,12 @@ return {
   config = function()
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
+    
 
-    keymap.set("n", "<leader>hx", "<cmd>lua require('harpoon.mark').add_file()<cr>", { desc = "Mark file with harpoon" })
-    keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
-    keymap.set("n", "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", { desc = "Go to previous harpoon mark" })
-    keymap.set("n", "<leader>ho", "<cmd>Telescope harpoon marks<cr>", { desc = "Go to all harpoon mark" })
+    keymap.set("n", "<F16>", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
+    keymap.set("n", "<F17>", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", { desc = "Go to previous harpoon mark" })
+    keymap.set("n", "<F18>", "<cmd>lua require('harpoon.mark').add_file()<cr>", { desc = "Mark file with harpoon" })
+    keymap.set("n", "<F19>", "<cmd>Telescope harpoon marks<cr>", { desc = "Go to all har<leader>hppoon mark" })
 
     require("telescope").load_extension('harpoon')
   end,
