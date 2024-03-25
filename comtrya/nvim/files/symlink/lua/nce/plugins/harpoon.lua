cc8bba1b8042e29e65114c6502e386efdffb0c63
Ulli Goschler <git@nce.wtf>
2024-03-25 15:51:32 +0100

    add harpoon config

--- comtrya/nvim/files/symlink/lua/nce/plugins/harpoon.lua
+++ comtrya/nvim/files/symlink/lua/nce/plugins/harpoon.lua
@@ -1,16 +1,37 @@
 return {
   "ThePrimeagen/harpoon",
+  branch = "harpoon2",
 
   config = function()
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-    
+    local harpoon = require("harpoon")
 
-    keymap.set("n", "<F16>", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
-    keymap.set("n", "<F17>", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", { desc = "Go to previous harpoon mark" })
-    keymap.set("n", "<F18>", "<cmd>lua require('harpoon.mark').add_file()<cr>", { desc = "Mark file with harpoon" })
-    keymap.set("n", "<F19>", "<cmd>Telescope harpoon marks<cr>", { desc = "Go to all har<leader>hppoon mark" })
+    harpoon:setup()
 
-    require("telescope").load_extension('harpoon')
+    vim.keymap.set("n", "<F16>", function() harpoon:list():append() end)
+    vim.keymap.set("n", "<C-[>", function() harpoon:list():remove() end)
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set("n", "<F18>", function() harpoon:list():prev() end)
+    vim.keymap.set("n", "<F19>", function() harpoon:list():next() end)
+    -- basic telescope configuration
+    local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+          results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+      }):find()
+    end
+
+    vim.keymap.set("n", "<F17>", function() toggle_telescope(harpoon:list()) end,
+      { desc = "Open harpoon window" })
   end,
 }
