b5ef8b124164d0e829b60ed9b4e1ef33bfcb3900
Ulli Goschler <git@nce.wtf>
2024-10-01 13:46:41 +0200

    add markdown support

--- comtrya/nvim/files/symlink/lua/nce/plugins/markdown.lua
+++ comtrya/nvim/files/symlink/lua/nce/plugins/markdown.lua
@@ -1,4 +1,14 @@
 return {
   "davidgranstrom/nvim-markdown-preview",
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {},
+    dependencies = { 
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons'
+    }, 
+  }
 }
+
+
 
